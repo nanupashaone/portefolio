8eec8e5486f966a0aad7a54604fea88172ef7d6d
nanupashaone <billy.tab@hotmail.com>
2023-03-20 16:32:35 +0100

    e

--- html-css/body/footer.php
+++ html-css/body/footer.php
@@ -1,14 +1,13 @@
-<!--
+
 
 <footer class="page-footer">
           
           
           <div class="footer-copyright" id="footer">
             <div class="container">
-            © 2022 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            © 2023 Copyright Text by Billy Tabonkeng
+            
             </div>
           </div>
         </footer>
 
-->        
